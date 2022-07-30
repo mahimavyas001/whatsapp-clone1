d59377ac7bd12c9ab76d2805676b45c5edce2f0c
mahimavyas001 <71896023+mahimavyas001@users.noreply.github.com>
2022-07-30 20:56:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+//import logo from './logo.svg';
+import "./App.css";
+import React from "react";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+//import { setPusherClient } from 'react-pusher';
+import Pusher from "pusher-js";
+import { useEffect, useState } from "react";
+import axios from "./axios";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
 
-  handleClick = api => e => {
-    e.preventDefault()
+function App() {
+  const [messages, setMessages ] = useState([]);
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
 
-  render() {
-    const { loading, msg } = this.state
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+    setMessages(response.data);
+    });
+    
+  }, []);
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+  useEffect(() => {
+    const pusher = new Pusher('4ecd1ebaf6f4150ab80d', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind('inserted', (newMessage) => {
+      //alert(JSON.stringify(newMessage));
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
+  console.log(messages);
+
+  return (
+    <div className="app">
+      <div className="app__body">
+    <Sidebar/>
+    <Chat messages={messages} />
+    </div>
+  </div>
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
